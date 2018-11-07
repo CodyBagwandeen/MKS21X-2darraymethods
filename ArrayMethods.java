6408e66f50a1df01db41e6462ee1ee31d1c098ef
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-11-06 20:17:16 -0500

    added a main and extra fuctions to test my functions ( borrowed this from timothy)

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,5 @@
 public class ArrayMethods{
 
-
   // Part 1
 
    public static int rowSum(int[][] ary, int x){
@@ -65,5 +64,77 @@ public class ArrayMethods{
      }
      return true;
    }
+
+
+   public static String printArray(int[] ary) {
+    String output = "[";
+    for(int i=0; i<ary.length-1; i++) {
+      output += ary[i] + ", ";
+    }
+
+    return output += ary[ary.length-1] + "]";
+
+  }
+
+  public static String printArray(int[][] ary) {
+    String output = "[" + printArray(ary[0]) + ",\n";
+    for(int i=1; i<ary.length-1; i++) {
+      output += " " + printArray(ary[i]) + ",\n";
+    }
+
+    return output += " " + printArray(ary[ary.length-1]) + "]";
+
+  }
+
+  public static void main(String[] args) {
+      int[][] a = {{1, 2, 3, 4}, {2, 1, 1, 2}, {4, 2}, {1, 3, 5}};
+      System.out.println("int[][] a : ");
+      System.out.println(printArray(a));
+      /*
+        [[1, 2, 3, 4],
+         [2, 1, 1, 2],
+         [4, 2],
+         [1, 3, 5]]
+      */
+
+      System.out.println();
+
+      System.out.println("rowSum(a,0) : " + rowSum(a,0));                        // 10
+      System.out.println("rowSum(a,2) : " + rowSum(a,2));                        // 6
+      System.out.println("rowSum(a,3) : " + rowSum(a,3));                        // 9
+      System.out.print("allRowSums(a) : " + printArray(allRowSums(a)) + "\n");   // [10, 6, 6, 9]
+
+      System.out.println();
+
+      System.out.println("columnSum(a,0) : " + columnSum(a,0));                  // 8
+      System.out.println("columnSum(a,2) : " + columnSum(a,2));                  // 9
+      System.out.println("columnSum(a,3) : " + columnSum(a,3));                  // 6
+      System.out.print("allColSums(a) : " + printArray(allColSums(a)) + "\n");   // [8, 8, 9, 6]
+
+      System.out.println();
+
+      int[][] b = {{1, 2, 3, 4}, {5, 5}, {2, 2, 4, 2}, {2, 1, 3, 4}};
+      System.out.println("int[][] b : ");
+      System.out.println(printArray(b));
+      /*
+        [[1, 2, 3, 4],
+         [5, 5],
+         [2, 2, 4, 2],
+         [2, 1, 3, 4]]
+      */
+
+      System.out.println();
+
+      System.out.println("isRowMagic(a) : " + isRowMagic(a));                    // false
+      System.out.println("isRowMagic(b) : " + isRowMagic(b));                    // true
+      System.out.print("allRowSums(b) : " + printArray(allRowSums(b)) + "\n");   // [10, 10, 10, 10]
+
+      System.out.println();
+
+      System.out.println("isColumnMagic(a) : " + isColumnMagic(a));              // false
+      System.out.println("isColumnMagic(a) : " + isColumnMagic(b));              // true
+      System.out.print("allColSums(b) : " + printArray(allColSums(b)) + "\n");   // [10, 10, 10, 10]
+
+    }
 
 }
